b2ff9d9d8c32c6683f24f3de077462cf29f7a69c
Robert Haritonov <r@rhr.me>
2016-11-08 16:42:29 +0100

    add build config

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var minimist = require('minimist');
-var args = minimist(process.argv.slice(2));
-
 var ftp = require('vinyl-ftp');
 var gutil = require('gulp-util');
-var minimist = require('minimist');
+var sass = require('gulp-sass');
+
 var args = minimist(process.argv.slice(2));
-gulp.task('deploy', function() {
+
+gulp.task('deploy', function () {
   var remotePath = '/domains/react.amsterdam/';
   var conn = ftp.create({
     host: 'indigo.elastictech.org',
@@ -17,4 +17,14 @@ gulp.task('deploy', function() {
   gulp.src(['./**/*.*', '!./node_modules'])
     .pipe(conn.newer(remotePath))
     .pipe(conn.dest(remotePath));
+});
+
+gulp.task('build', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
+gulp.task('dev', function () {
+  gulp.watch('./sass/**/*.scss', ['build']);
 });
