0b38bfb28d8d48994586e8fc97228a5d8a9a7885
Oleg Shelest <lateexairu@gmail.com>
2017-12-01 02:14:11 +0200

    styles fixes

--- js/scripts.js
+++ js/scripts.js
@@ -2,29 +2,40 @@ $(function() {
 
 	// Burger menu
 
-	$('.menu-toggle').click(function(){
-		$(this).toggleClass('open');
-		$(this).siblings('.main-nav').toggleClass('open');
-		$(this).closest('.wrapper').find('.content').toggleClass('fade');
+	$('.menu-toggle, .tint').click(function(){
+		$(this).closest('.wrapper').find('.menu-toggle, .main-nav').toggleClass('open');
+		$(this).closest('.wrapper').children('.tint').toggleClass('visible');
+		$(this).closest('.wrapper').find('.content, .footer').toggleClass('fade');
 	});
+
+	// $(document).click(function(event) { 
+	// 	if(!$(event.target).closest('.main-nav.open').length) {
+	// 		if($('.main-nav').is(":visible")) {
+	// 			console.log('aaa');
+				
+	// 			$('.main-nav').removeClass('open');
+	// 			$('.wrapper').find('.content, .footer').removeClass('fade');
+	// 		}
+	// 	}      
+	// })
 
 	// Anchor navigation
 
 	$('a[href*="#"]:not([href="#"])').click(function () {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      var _this = this;
-      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 400, function() {
-          location.hash = _this.hash;
-        });
-        return false;
-      }
-    }
-  });
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			var _this = this;
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+			$('html, body').animate({
+				scrollTop: target.offset().top
+			}, 400, function() {
+				location.hash = _this.hash;
+			});
+			return false;
+			}
+		}
+	});
 
 	// Tabs
 
