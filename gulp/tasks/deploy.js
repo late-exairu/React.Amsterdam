66b88f74fd2a63e153714494c8c5c60f4aa915b5
Robert Haritonov <r@rhr.me>
2019-11-07 10:13:24 +0000

    optimize CI cache and FTP deploy

--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -17,12 +17,14 @@ gulp.task('deploy', function() {
   gulp.src([
     './build/**/*.*'
   ])
+    .pipe(conn.differentSize(remotePath))
     .pipe(conn.dest(remotePath));
 
   gulp.src([
     './sponsors/**/*.*',
     '!./sponsors/node_modules/**/*.*',
   ])
+    .pipe(conn.differentSize(remotePath))
     .pipe(conn.dest(`${remotePath}/sponsors`));
 
   // uncomment to deploy last year versions
