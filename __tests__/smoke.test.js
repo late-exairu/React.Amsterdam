82d8ef48e210cb8965bccd632cc8ec19517e438c
Oleg Proskurin <regx@usul.su>
2020-01-17 12:06:31 +0300

    Add Snapshot Tests

--- __tests__/smoke.test.js
+++ __tests__/smoke.test.js
@@ -1,11 +1,9 @@
-import regeneratorRuntime from "regenerator-runtime";
+import regeneratorRuntime from 'regenerator-runtime';
 // import { getContent } from '@focus-reactive/graphql-content-layer';
 var { getContent } = require('../content');
 
-describe('JsNation', () => {
+describe('Content layer', () => {
   it('should render content', async () => {
-    const content = await getContent();
-
-    expect(content).toMatchSnapshot();
+    const result = await getContent();
   });
 });
